ecfbe39f88000e1727e275b99b4dc4e69af26b1d
DaneCook <danepcook@gmail.com>
2019-10-20 14:04:38 -0400

    added data for graphs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const alert = document.getElementById('alert');
 const trafficCanvas = document.getElementById('traffic-chart');
+const dailyCanvas = document.getElementById('daily-chart');
+const mobileCanvas = document.getElementById('mobile-chart');
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
 
 
 
@@ -34,3 +39,97 @@ let trafficData = {
     borderWidth: 1,
   }]
 };
+
+let trafficOptions = {
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0
+  },
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero:true
+      }
+    }]
+  },
+  legend : {
+    display: false
+  }
+};
+
+let trafficChart = new Chart(trafficCanvas, {
+  type: 'line',
+  data: trafficData,
+  options: trafficOptions
+});
+
+const dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [{
+    label: '# of hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
+  }]
+};
+
+const dailyOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  },
+  legend: {
+    display: false
+  }
+}
+
+let dailyChart = new Chart(dailyCanvas, {
+  type: 'bar',
+  data: dailyData,
+  options: dailyOptions
+});
+
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [{
+    label: "# of Users",
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+      '#7477BF',
+      '#78CF82',
+      '#51B6C8'
+    ]
+  }]
+};
+
+const mobileOptions = {
+  legend: {
+    position: 'right',
+    labels: {
+      boxWidth: 20,
+      fontStyle: 'bold'
+    }
+  }
+}
+
+let mobileChart = new Chart(mobileCanvas, {
+  type: 'doughnut',
+  data: mobileData,
+  options: mobileOptions
+});
+
+send.addEventListener('click', () => {
+  if (user.value === '' && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.value === "") {
+    alert("Please fill out user field before sending");
+  } else if (measse.value === "") {
+    alert("Please fill out message field before sending");
+  } else {
+    alert("Message successfully sent to: ${user.value}");
+  }
+});
